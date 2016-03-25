0d3ae7488209accf71f2d1845dbfccc9ae935052
Adam Gastineau <adam@appcannonsoftware.com>
2016-03-25 16:43:06 -0400

    Added helpers for common stack and PC operations, along with simple error checking Added stack instructions

--- SimpleNES/Implementation/CPU.swift
+++ SimpleNES/Implementation/CPU.swift
@@ -25,7 +25,7 @@ class CPU: NSObject {
 	/**
 	 Stack Pointer
 	*/
-	var SP: UInt16;
+	var SP: UInt8;
 
 	/**
 	 Processor Status
@@ -69,11 +69,6 @@ class CPU: NSObject {
         self.ppuMemory = ppuMemory;
     }
     
-    func setPC(address: UInt16) {
-        self.PCL = UInt8(address & 0xFF);
-        self.PCH = UInt8((address & 0xFF00) >> 8);
-    }
-    
     func reset() {
         // Load program start address from RESET vector (0xFFFC)
         let programStartAddress = self.mainMemory.readTwoBytesMemory(0xFFFC);
@@ -90,4 +85,144 @@ class CPU: NSObject {
 	func step() {
 		
 	}
+    
+    // MARK: PC Operations
+    func setPC(address: UInt16) {
+        self.PCL = UInt8(address & 0xFF);
+        self.PCH = UInt8((address & 0xFF00) >> 8);
+    }
+    
+    func getPC() -> UInt16 {
+        return UInt16(self.PCL & 0xFF) | ((UInt16(self.PCH) & UInt16(0xFF00)) >> 8);
+    }
+    
+    func incrementPC() {
+        setPC(getPC() + 1);
+    }
+    
+    func decrementPC() {
+        setPC(getPC() - 1);
+    }
+    
+    func fetchPC() -> UInt8 {
+        let byte = self.mainMemory.readMemory(Int(getPC()));
+        
+        incrementPC();
+        
+        return byte;
+    }
+    
+    // MARK: Stack Operations
+    func push(byte: UInt8) {
+        self.mainMemory.writeMemory(0x100 + Int(self.SP), data: byte);
+        
+        if(self.SP == 0xFF) {
+            print("ERROR: Stack overflow");
+        }
+        
+        self.SP = self.SP + 1;
+    }
+    
+    func pop() -> UInt8 {
+        if(self.SP == 0) {
+            print("ERROR: Stack underflow");
+        }
+        
+        self.SP = self.SP - 1;
+        
+        return self.mainMemory.readMemory(0x100 + Int(self.SP));
+    }
+    
+    // MARK: Instructions
+    // MARK: Stack
+    
+    /**
+     Simulate Interrupt ReQuest (IRQ)
+    */
+    func BRK() -> Int {
+        // TODO: Set B flag
+        push(self.PCH);
+        push(self.PCL);
+        push(self.P);
+        
+        self.PCL = self.mainMemory.readMemory(0xFFFE);
+        self.PCH = self.mainMemory.readMemory(0xFFFF);
+        
+        return 7;
+    }
+    
+    /**
+     ReTurn from Interrupt
+    */
+    func RTI() -> Int {
+        self.P = pop();
+        self.PCL = pop();
+        self.PCH = pop();
+        
+        return 6;
+    }
+    
+    /**
+     ReTurn from Subroutine
+    */
+    func RTS() -> Int {
+        self.PCL = pop();
+        self.PCH = pop();
+        
+        incrementPC();
+        
+        return 6;
+    }
+    
+    /**
+     PusH A
+    */
+    func PHA() -> Int {
+        push(self.A);
+        
+        return 3;
+    }
+    
+    /**
+     PusH P
+    */
+    func PHP() -> Int {
+        push(self.P);
+        
+        return 3;
+    }
+    
+    /**
+     PulL from Stack to A
+    */
+    func PLA() -> Int {
+        self.A = pop();
+        
+        return 4;
+    }
+    
+    /**
+     PulL from Stack to P
+    */
+    func PLP() -> Int {
+        self.P = pop();
+        
+        return 4;
+    }
+    
+    /**
+     Jump to SubRoutine
+    */
+    func JSR() -> Int {
+        let lowByte = fetchPC();
+        
+        // TODO: Possibly incorrect?
+        push(self.PCH);
+        push(self.PCL);
+        
+        self.PCL = lowByte;
+        self.PCH = fetchPC();
+        
+        return 6;
+    }
 }
