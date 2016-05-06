9635b49c55d9fd9c6c77ed727b0f408ddd960763
Adam Gastineau <adam@appcannonsoftware.com>
2016-05-06 12:29:39 -0400

    Added frame counter operation

--- SimpleNES/Implementation/APU.swift
+++ SimpleNES/Implementation/APU.swift
@@ -12,7 +12,26 @@ final class APU {
 	
 	// MARK: - Class declarations
 	
-	final class Square {
+	private class APURegister {
+		/*
+		Register 4 bits
+		*/
+		// 3 bits
+		var msbWavelength: UInt8;
+		// 5 bits
+		var lengthCounterLoad: UInt8;
+		
+		init() {
+			self.msbWavelength = 0;
+			self.lengthCounterLoad = 0;
+		}
+		
+		func stepLength() {
+			
+		}
+	}
+	
+	private class Square: APURegister {
 		
 		/*
 			Register 1 bits
@@ -41,15 +60,7 @@ final class APU {
 		*/
 		var lsbWavelength: UInt8;
 		
-		/*
-			Register 4 bits
-		*/
-		// 3 bits
-		var msbWavelength: UInt8;
-		// 5 bits
-		var lengthCounterLoad: UInt8;
-		
-		init() {
+		override init() {
 			self.volume = 0;
 			self.envelopeDisable = false;
 			self.lengthCounterDisable = false;
@@ -61,13 +72,18 @@ final class APU {
 			self.sweepEnable = false;
 			
 			self.lsbWavelength = 0;
+		}
+		
+		func stepSweep() {
 			
-			self.msbWavelength = 0;
-			self.lengthCounterLoad = 0;
+		}
+		
+		func stepEnvelope() {
+			
 		}
 	}
 	
-	final class Triangle {
+	private class Triangle: APURegister {
 		
 		/*
 			Register 1 bits
@@ -81,26 +97,19 @@ final class APU {
 		*/
 		var lsbWavelength: UInt8;
 		
-		/*
-			Register 4 bits
-		*/
-		// 3 bits
-		var msbWavelength: UInt8;
-		// 5 bits
-		var lengthCounterLoad: UInt8;
-		
-		init() {
+		override init() {
 			self.linearCounterLoad = 0;
 			self.lengthCounterClockDisable = false;
 			
 			self.lsbWavelength = 0;
+		}
+		
+		func stepLinear() {
 			
-			self.msbWavelength = 0;
-			self.lengthCounterLoad = 0;
 		}
 	}
 	
-	final class Noise {
+	private class Noise: APURegister {
 		
 		/*
 			Register 1 bits
@@ -123,14 +132,9 @@ final class APU {
 		
 		var randomNumberGeneration: Bool;
 		
-		/*
-			Register 4 bits
-		*/
-		// 3 unused bits
-		// 5 bits
-		var lengthCounterLoad: UInt8;
+		// 3 unused bits in register 4 (msbWavelength)
 		
-		init() {
+		override init() {
 			self.volume = 0;
 			self.envelopeDisable = false;
 			self.lengthCounterDisable = false;
@@ -138,14 +142,108 @@ final class APU {
 			
 			self.sampleRate = 0;
 			self.randomNumberGeneration = false;
-			
-			self.lengthCounterLoad = 0;
 		}
+		
+		func stepEnvelope() {
+			
+		}
+	}
+	
+	// MARK: - APU Registers
+	
+	private var timerControl:UInt8 {
+		didSet {
+			self.disableIRQ = timerControl & 0x40 == 0x40;
+			self.framerateSwitch = timerControl & 0x80 == 0x80;
+			
+			if(self.framerateSwitch) {
+				self.frameCount = 0;
+			} else {
+				self.frameCount = 4;
+			}
+		}
+	}
+	
+	private var disableIRQ: Bool;
+	
+	/**
+		If true, 5 frames occur in each frame counter cycle, otherwise 4
+	*/
+	private var framerateSwitch: Bool;
+	
+	// MARK: - APU Variables
+	
+	private let square1: Square;
+	private let square2: Square;
+	private let triangle: Triangle;
+	private let noise: Noise;
+	
+	private var cycle: Int;
+	private var frameCount: Int;
+	
+	init() {
+		self.timerControl = 0;
+		self.disableIRQ = false;
+		self.framerateSwitch = false;
+		
+		self.square1 = Square();
+		self.square2 = Square();
+		self.triangle = Triangle();
+		self.noise = Noise();
+		
+		self.cycle = 0;
+		self.frameCount = 4;
 	}
 	
 	// MARK: - APU Functions
 	
 	func step() {
 		
+	}
+	
+	func stepFrame() {
+		if(self.framerateSwitch) {
+			self.frameCount = (self.frameCount + 1) % 5;
+		} else {
+			self.frameCount = (self.frameCount + 1) % 4;
+		}
+		
+		switch(self.frameCount) {
+			case 0, 2:
+				stepEnvelope();
+			case 1, 3:
+				stepEnvelope();
+				
+				stepSweep();
+				
+				stepLength();
+				break;
+			case 4:
+				break;
+			default:
+				break;
+		}
+	}
+	
+	func stepEnvelope() {
+		// Increment envelope (Square and Noise)
+		self.square1.stepEnvelope();
+		self.square2.stepEnvelope();
+		self.triangle.stepLinear();
+		self.noise.stepEnvelope();
+	}
+	
+	func stepSweep() {
+		// Increment frequency sweep (Square)
+		self.square1.stepSweep();
+		self.square2.stepSweep();
+	}
+	
+	func stepLength() {
+		// Increment length counters (all)
+		self.square1.stepLength();
+		self.square2.stepLength();
+		self.triangle.stepLength();
+		self.noise.stepLength();
 	}
 }
